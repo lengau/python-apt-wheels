e723f8b160879bcfe7c192ec383fcd4f03ef5837
Alex Lowe <alex.lowe@canonical.com>
2024-12-07 20:09:23 -0500

    fix: include architecture in platform name.

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ cd python-apt-*
 export GLIBC_VERSION="$(ldd --version | grep GLIBC | cut -d' ' -f4 | cut -d- -f1 | tr . _)"
 export DEBVER="$(apt-cache show python3-apt|grep Version | head -n 1|cut -d' ' -f2)"
 
-python3 setup.py bdist_wheel --dist-dir /github/workspace --plat-name "manylinux_$GLIBC_VERSION"
+python3 setup.py bdist_wheel --dist-dir /github/workspace --plat-name "manylinux_$GLIBC_VERSION_$(uname -m)"
