4e2a03a085e53389906b118d3e9e7e87d98c7f22
Alex Lowe <alex.lowe@canonical.com>
2024-12-07 19:37:46 -0500

    feat: initial version with jammy

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /tmp
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get source python-apt
+cd python-apt-*
+
+export GLIBC_VERSION="$(ldd --version | grep GLIBC | cut -d' ' -f4 | cut -d- -f1 | tr . _)"
+export DEBVER="$(apt-cache show python3-apt|grep Version | head -n 1|cut -d' ' -f2)"
+
+python3 setup.py bdist_wheel --dist-dir /github/workspace --plat-name "manylinux_$GLIBC_VERSION"
