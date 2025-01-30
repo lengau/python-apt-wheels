558c08896b05d7e79724d4b3224f80d8f1bdc2da
Alex Lowe <alex.lowe@canonical.com>
2025-01-30 16:32:08 -0500

    fix: updates

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 cd /tmp
 
@@ -11,4 +12,9 @@ cd python-apt-*
 export GLIBC_VERSION="$(ldd --version | grep GLIBC | cut -d' ' -f4 | cut -d- -f1 | tr . _)"
 export DEBVER="$(apt-cache show python3-apt|grep Version | head -n 1|cut -d' ' -f2)"
 
-python3 setup.py bdist_wheel --dist-dir /github/workspace --plat-name "manylinux_${GLIBC_VERSION}_$(uname -m)"
+for UV_PYTHON in 3.10 3.11 3.12 3.13; do
+    /root/.local/bin/uv venv
+    /root/.local/bin/uv pip install setuptools
+    source .venv/bin/activate
+    python3 setup.py bdist_wheel --dist-dir /github/workspace --plat-name "manylinux_${GLIBC_VERSION}_$(uname -m)"
+done
