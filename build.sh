7c7289e168a89bfc913227b6b591f85fa062ec8b
Alex Lowe <alex.lowe@canonical.com>
2025-01-31 18:20:03 -0500

    fix: build for  multiple python versions

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ export GLIBC_VERSION="$(ldd --version | grep GLIBC | cut -d' ' -f4 | cut -d- -f1
 export DEBVER="$(apt-cache show python3-apt|grep Version | head -n 1|cut -d' ' -f2)"
 
 for UV_PYTHON in 3.10 3.11 3.12 3.13; do
+    export UV_PYTHON
     /root/.local/bin/uv venv
     /root/.local/bin/uv pip install setuptools
     source .venv/bin/activate
